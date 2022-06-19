a567dcd05822ff5b987f9a604ee3c1bc67ad16c8
bayustrio7 <bayustrio7@gmail.com>
2022-06-19 16:15:43 +0700

    dekete markdown

--- components/Markdown/Editor.tsx
+++ components/Markdown/Editor.tsx
@@ -1,55 +1,7 @@
-import { Dispatch, MutableRefObject, SetStateAction } from "react";
-import dynamic from "next/dynamic";
-import "react-markdown-editor-lite/lib/index.css";
-import { IStory } from "../../Types/Story-type";
-import MarkdownIt from "markdown-it";
-const MdEditor = dynamic(() => import("react-markdown-editor-lite"), {
-  ssr: false,
-});
+import React from "react";
 
-interface IProps {
-  content: string;
-  story: IStory;
-  setStory: Dispatch<SetStateAction<IStory>>;
-}
-
-const Editor = ({ setStory, story }: IProps) => {
-  const mdParser = new MarkdownIt();
-
-  const handleEditorChange = ({ text }: { text: string }) => {
-    console.log("handleEditorChange", text);
-    setStory({ ...story, content: text });
-  };
-
-  const onCustomImageUpload = (file: File) => {
-    return new Promise((resolve) => {
-      const reader = new FileReader();
-      reader.onload = (data) => {
-        resolve(data.target?.result);
-      };
-      reader.readAsDataURL(file);
-    });
-  };
-
-  return (
-    <div>
-      <MdEditor
-        style={{ height: "500px" }}
-        config={{
-          view: {
-            menu: true,
-            md: true,
-            html: true,
-            fullScreen: true,
-            hideMenu: true,
-          },
-        }}
-        onChange={handleEditorChange}
-        onImageUpload={onCustomImageUpload}
-        renderHTML={(text) => mdParser.render(text)}
-      />
-    </div>
-  );
+const Editor = () => {
+  return <div>Editor</div>;
 };
 
 export default Editor;
